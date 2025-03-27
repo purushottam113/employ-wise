aaf7f4ea4c5ef3a8dc54ce2a9bee90d0290c5de7
purushottam113 <shreepad113@gmail.com>
2025-03-27 20:00:41 +0530

    setup vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./componants/Navbar"
+
+function App() {
+
+  return (
+    <>
+     <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/componants/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between p-5 bg-amber-200'>
+      <p className=''>EMPLOY WISE</p>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default Navbar
